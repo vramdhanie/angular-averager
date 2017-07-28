f3ce6429199862919c9d0c9f6f1436d7d5f39798
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-07-28 12:54:25 -0400

    Add timer component

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss']
+})
+export class TimerComponent implements OnInit {
+
+  @Input() time: Number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
