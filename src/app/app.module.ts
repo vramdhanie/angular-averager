bcc24c8b6fde8c807fea35a1a0b7ca78a30e9d2d
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-07-29 19:30:05 -0400

    Add estimated time component to main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
 import { ItemCountComponent } from './item-count/item-count.component';
+import { EstimatedTimeComponent } from './estimated-time/estimated-time.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimerComponent,
-    ItemCountComponent
+    ItemCountComponent,
+    EstimatedTimeComponent
   ],
   imports: [
     BrowserModule
