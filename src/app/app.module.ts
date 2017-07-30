4fcc26f6bcc558b7f78d429a83d057bebf6f094a
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-07-29 21:30:38 -0400

    Add controls to main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
 import { ItemCountComponent } from './item-count/item-count.component';
 import { EstimatedTimeComponent } from './estimated-time/estimated-time.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimerComponent,
     ItemCountComponent,
-    EstimatedTimeComponent
+    EstimatedTimeComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule
